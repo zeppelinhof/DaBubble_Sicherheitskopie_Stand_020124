720d10954638f2d30056df3ca80fdae3ee10e79e
Laura Hesidenz <l.hesidenz97@gmail.com>
2023-12-03 17:27:01 +0100

    small changes

--- src/app/shared/services/user-session-check.service.ts
+++ src/app/shared/services/user-session-check.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class UserSessionCheckService {
-  userIsLogged: boolean = false;
+  userIsLogged: boolean = true;
 
   constructor() {}
 }
