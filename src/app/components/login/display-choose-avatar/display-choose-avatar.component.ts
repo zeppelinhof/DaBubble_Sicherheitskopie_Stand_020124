3e61db174e8dc0ef36b450eef9c769b8469aa943
Laura Hesidenz <l.hesidenz97@gmail.com>
2023-12-03 17:26:21 +0100

    style create account

--- src/app/components/login/display-choose-avatar/display-choose-avatar.component.ts
+++ src/app/components/login/display-choose-avatar/display-choose-avatar.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-display-choose-avatar',
   templateUrl: './display-choose-avatar.component.html',
-  styleUrls: ['./display-choose-avatar.component.scss']
+  styleUrls: ['./display-choose-avatar.component.scss'],
 })
 export class DisplayChooseAvatarComponent {
+  arrowBackIsHovered: boolean = false;
 
+  avatarImages = [
+    'userFemale1.png',
+    'userMale3.png',
+    'userMale1.png',
+    'userMale2.png',
+    'userMale4.png',
+    'userFemale2.png',
+  ];
 }
