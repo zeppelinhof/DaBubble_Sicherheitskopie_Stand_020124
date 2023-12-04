73f1b6f163d12362c79ef014b1e1899ec746bb06
Laura Hesidenz <l.hesidenz97@gmail.com>
2023-12-04 22:51:31 +0100

    imprint

--- src/app/components/imprint/imprint.component.ts
+++ src/app/components/imprint/imprint.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-imprint',
   templateUrl: './imprint.component.html',
-  styleUrls: ['./imprint.component.scss']
+  styleUrls: ['./imprint.component.scss'],
 })
 export class ImprintComponent {
+  arrowBackIsHovered: boolean = false;
 
+  constructor(private router: Router) {
+    this.handleBackButton();
+  }
+
+  private handleBackButton() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        //hier hinzufügen, was passieren soll, wenn Route geändert wurde - nichts
+      }
+    });
+  }
 }
--- src/app/shared/services/user-session-check.service.ts
+++ src/app/shared/services/user-session-check.service.ts
@@ -4,7 +4,5 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class UserSessionCheckService {
-  userIsLogged: boolean = true;
-
-  constructor() {}
+  userIsLogged: boolean = false;
 }
