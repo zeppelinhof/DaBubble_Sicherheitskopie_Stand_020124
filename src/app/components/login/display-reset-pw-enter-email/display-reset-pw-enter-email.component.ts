977f131947abc82a24483e1544762ccaed98982f
Laura Hesidenz <l.hesidenz97@gmail.com>
2023-12-04 19:10:32 +0100

    reset pw

--- src/app/components/login/display-reset-pw-enter-email/display-reset-pw-enter-email.component.ts
+++ src/app/components/login/display-reset-pw-enter-email/display-reset-pw-enter-email.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-display-reset-pw-enter-email',
   templateUrl: './display-reset-pw-enter-email.component.html',
-  styleUrls: ['./display-reset-pw-enter-email.component.scss']
+  styleUrls: ['./display-reset-pw-enter-email.component.scss'],
 })
 export class DisplayResetPwEnterEmailComponent {
-
+  arrowBackIsHovered: boolean = false;
 }
