f39562157904ad991ffa7ff8a57b313228d959e5
Markus Gede <markusgede93@gmail.com>
2023-12-06 17:02:43 +0100

    input gets current channel

--- src/app/components-sub/input-field-channel/input-field-channel.component.ts
+++ src/app/components-sub/input-field-channel/input-field-channel.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Channel } from 'src/app/interfaces/channel';
+import { ChannelService } from 'src/app/shared/services/channel.service';
 import { InputService } from 'src/app/shared/services/input.service';
 
 @Component({
@@ -7,6 +9,16 @@ import { InputService } from 'src/app/shared/services/input.service';
   styleUrls: ['./input-field-channel.component.scss']
 })
 export class InputFieldChannelComponent {
+  clickedChannel!: Channel;
   
-  constructor(public service: InputService){}
+  constructor(public service: InputService, public cs: ChannelService){}
+
+  ngOnInit(): void {
+    this.cs.clickedChannel
+      .subscribe((ch: Channel) => {
+        this.clickedChannel = ch;
+        console.log('Komponente Input Channel hat folgenden Channel erhalten:', ch)
+      });
+  }
+  
 }
