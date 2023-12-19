f886922dbe26e2c9c277e6dd653f5b8d491aaeb6
Laura Hesidenz <l.hesidenz97@gmail.com>
2023-12-20 00:21:24 +0100

    added auth to header

--- src/app/components/dashboard/header/header.component.ts
+++ src/app/components/dashboard/header/header.component.ts
@@ -8,15 +8,9 @@ import { UserService } from 'src/app/shared/services/user.service';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-<<<<<<< HEAD
   constructor(public us: UserService, public auth: AuthenticationService) {}
-=======
 
-
-  constructor(public us: UserService){}
-
-  reloadPage(){
+  reloadPage() {
     window.location.reload();
   }
->>>>>>> 795e090ce3ebe474e2fba9deb4c94037ae021552
 }
