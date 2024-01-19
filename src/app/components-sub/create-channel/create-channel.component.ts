a4dae0585d0de73dba497a061066b8c5338173be
Markus Gede <markusgede93@gmail.com>
2024-01-19 15:24:15 +0100

    max channel name length (20 chars)

--- src/app/components-sub/create-channel/create-channel.component.ts
+++ src/app/components-sub/create-channel/create-channel.component.ts
@@ -10,15 +10,27 @@ import { SearchInputService } from 'src/app/shared/services/search-input.service
   styleUrls: ['./create-channel.component.scss'],
 })
 export class CreateChannelComponent {
+  showLengthInfo: boolean = false;
+  maxNameLength: number = 20;
 
   constructor(
     public ws: WorkspaceService,
-    public cs: ChannelService,    
+    public cs: ChannelService,
     public sis: SearchInputService,
     public us: UserService
-  ) {}  
+  ) { }
 
   changeRadioButton() {
     return (this.ws.radioButtonFirst = this.ws.radioButtonFirst ? false : true);
+  }
+
+  onInput() {
+    this.ws.inputName = this.ws.inputName.slice(0, this.maxNameLength); // Begrenzt auf 20 Zeichen
+    this.sis.allFieldsFilled(this.us.userLoggedIn());
+    this.showLengthInfo = this.ws.inputName.length === this.maxNameLength;
+
+    setTimeout(()=>{
+      this.showLengthInfo = false;
+    }, 10000);
   }
 }
