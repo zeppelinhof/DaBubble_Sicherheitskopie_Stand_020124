08cbe0e3ba9ab64e1da1aea8314160cd5acb0196
LauraHexx <l.hesidenz97@gmail.com>
2024-01-25 19:14:07 +0100

    deleted change email

--- src/app/components-sub/settings-edit-user-data/settings-edit-user-data.component.ts
+++ src/app/components-sub/settings-edit-user-data/settings-edit-user-data.component.ts
@@ -19,56 +19,23 @@ export class SettingsEditUserDataComponent {
   ) {}
 
   changeUserDataForm: any = new FormGroup({
-    email: new FormControl('', [Validators.email]),
     name: new FormControl('', [Validators.minLength(3)]),
   });
-
-  /**
-   * Checks if the email in the signup form contains a dot. Firebase requires a dot in the domain part of the email.
-   */
-  checkInputEmailContainsDot(): boolean {
-    const emailInputField = this.changeUserDataForm.get('email');
-    if (emailInputField) {
-      const email = emailInputField.value;
-      const parts = email.split('@');
-      return parts.length === 2 && parts[1].includes('.');
-    } else {
-      return false;
-    }
-  }
-
-  /**
-   * Checks if the user with the provided signup email already exists.
-   */
-  userIsAlreadyExisting(): boolean {
-    const emailInputField = this.changeUserDataForm.get('email').value;
-    const emailExists = this.userService.myUsers.some(
-      (user) => user.email === emailInputField
-    );
-    return emailExists;
-  }
 
   /**
    * Checks if no data has been entered in the user data form.
    */
   noDataEntered() {
-    const emailValue = this.changeUserDataForm.get('email')?.value;
     const nameValue = this.changeUserDataForm.get('name')?.value;
-    return !this.changeUserDataForm.valid || (!emailValue && !nameValue);
+    return !this.changeUserDataForm.valid || nameValue;
   }
 
   /**
    * Asynchronously updates user data based on the entered values in the form.
    */
   async changeUserData() {
-    const emailValue = this.changeUserDataForm.get('email')?.value;
     const nameValue = this.changeUserDataForm.get('name')?.value;
     const currentUser = this.userService.userLoggedIn();
-
-    if (emailValue) {
-      await this.userService.updateUser({ email: emailValue }, currentUser);
-      //await this.auth.updateUserEmail(emailValue);
-    }
     if (nameValue) {
       await this.userService.updateUser({ name: nameValue }, currentUser);
     }
--- src/app/shared/services/authentication.service.ts
+++ src/app/shared/services/authentication.service.ts
@@ -12,6 +12,7 @@ import {
   sendPasswordResetEmail,
   updateEmail,
   getRedirectResult,
+  verifyBeforeUpdateEmail,
 } from 'firebase/auth';
 
 import {
@@ -210,9 +211,25 @@ export class AuthenticationService {
     this.router.navigate(['/login']);
   }
 
+  async updateUserEmail(newEmail: string) {
+    const auth = getAuth();
+    const user = auth.currentUser;
+    if (user) {
+      try {
+        await verifyBeforeUpdateEmail(user, newEmail);
+        window.location.reload();
+      } catch (error: any) {
+        console.log(error.message);
+      }
+    }
+  }
+
+  /*
   updateUserEmail(newEmail: string) {
     const auth = getAuth();
     const user = auth.currentUser;
+    console.log('test');
+
     if (user) {
       updateEmail(user, newEmail)
         .then(() => {
@@ -221,4 +238,5 @@ export class AuthenticationService {
         .catch((error) => {});
     }
   }
+  */
 }
