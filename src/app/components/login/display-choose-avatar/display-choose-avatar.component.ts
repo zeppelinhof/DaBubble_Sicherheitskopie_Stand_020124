4400324b6b5c779a0e80e9f8d3073bc3ddda2d0b
LauraHexx <l.hesidenz97@gmail.com>
2024-01-29 19:56:22 +0100

    animations responsiv

--- src/app/components/login/display-choose-avatar/display-choose-avatar.component.ts
+++ src/app/components/login/display-choose-avatar/display-choose-avatar.component.ts
@@ -3,6 +3,7 @@ import { AuthenticationService } from './../../../shared/services/authentication
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { StorageService } from 'src/app/shared/services/storage.service';
+import { AnimationsService } from 'src/app/shared/services/animations.service';
 
 @Component({
   selector: 'app-display-choose-avatar',
@@ -32,7 +33,8 @@ export class DisplayChooseAvatarComponent {
   constructor(
     private auth: AuthenticationService,
     private router: Router,
-    public storService: StorageService
+    public storService: StorageService,
+    public animations: AnimationsService
   ) {
     this.checkDataLocalStorage();
   }
@@ -70,7 +72,7 @@ export class DisplayChooseAvatarComponent {
       this.newUser.img = this.storService.channelCurrentUrl;
     }
     this.deleteLocalStorage();
-    this.newUserSuccess = true;
+    this.animations.newUserSuccess = true;
     setTimeout(() => {
       this.auth.signUp(this.newUser, this.password);
     }, 680);
